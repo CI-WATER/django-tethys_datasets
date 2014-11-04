bf906f38a231dfe8500a1815888ec4dd3b55e95f
Nathan Swain <nathan.swain86@gmail.com>
2014-11-04 16:42:38 -0700

    Create and update methods implemented for datasets and delete methods implemented for datasets and resources.

--- tethys_datasets/engines/ckan_engine.py
+++ tethys_datasets/engines/ckan_engine.py
@@ -220,7 +220,7 @@ class CKANDatasetEngine(DatasetEngineABC):
         (http://docs.ckan.org/en/ckan-2.2/api.html).
 
         Args:
-          resource_id (string): The id or name of the resource to retrieve.
+          resource_id (string): The id of the resource to retrieve.
           console (bool, optional): Pretty print the result to the console for debugging. Defaults to False.
           **kwargs: Any number of optional keyword arguments for the method (see CKAN docs).
 
@@ -242,38 +242,156 @@ class CKANDatasetEngine(DatasetEngineABC):
                                            error={'404': error_404})
         return result
 
-    def create_dataset(self, console=False, **kwargs):
+    def create_dataset(self, name, console=False, **kwargs):
         """
         Create CKAN dataset
-        :return:
-        """
 
-    def create_resource(self, console=False, *kwargs):
+        Wrapper for the CKAN package_create API method. See the CKAN API docs for this method to see applicable options
+        (http://docs.ckan.org/en/ckan-2.2/api.html).
+
+        Args:
+          name (string): The id or name of the resource to retrieve.
+          console (bool, optional): Pretty print the result to the console for debugging. Defaults to False.
+          **kwargs: Any number of optional keyword arguments for the method (see CKAN docs).
+
+        Returns:
+          The response dictionary or None if an error occurs.
+        """
+        # Assemble the data dictionary
+        data = kwargs
+        data['name'] = name
+
+        # Execute
+        result = self._execute_api_request(method='package_create',
+                                           data_dictionary=data,
+                                           console=console,)
+        return result
+
+    def create_resource(self, dataset, url, console=False, *kwargs):
         """
         Create CKAN resource
-        :return:
         """
+        # TODO: Implement this method fully to allow file uploads.
 
-    def update_dataset(self, console=False, **kwargs):
+    def update_dataset(self, dataset_id, console=False, **kwargs):
         """
         Update CKAN dataset
-        :return:
-        """
 
-    def update_resource(self, console=False, **kwargs):
+        Wrapper for the CKAN package_update API method. See the CKAN API docs for this method to see applicable options
+        (http://docs.ckan.org/en/ckan-2.2/api.html).
+
+        Args:
+          dataset_id (string): The id or name of the dataset to update.
+          console (bool, optional): Pretty print the result to the console for debugging. Defaults to False.
+          **kwargs: Any number of optional keyword arguments for the method (see CKAN docs).
+
+        Returns:
+          The response dictionary or None if an error occurs.
+        """
+        # Assemble the data dictionary
+        data = kwargs
+        data['id'] = dataset_id
+
+        # Preserve the resources and tags if not included in parameters
+        """
+        Note: The default behavior of the package_update is to replace the resources and tags attributes with empty
+              lists if they are not included in the parameter list... This is suboptimal, because the resources become
+              disassociated with the dataset and float off into the ether. This behavior is modified in this method so
+              that these properties are retained by default, unless included in the parameters that are being updated.
+        """
+        result = self._execute_api_request(method='package_show',
+                                           data_dictionary=data)
+        if result['success']:
+            original_dataset = result['result']
+
+            if 'resources' not in data:
+                data['resources'] = original_dataset['resources']
+
+            if 'tags' not in data:
+                data['tags'] = original_dataset['tags']
+
+
+        # Execute
+        result = self._execute_api_request(method='package_update',
+                                           data_dictionary=data,
+                                           console=console)
+
+        return result
+
+    def update_resource(self, resource_id, console=False, **kwargs):
         """
         Update CKAN resource
-        :return:
-        """
 
-    def delete_dataset(self, console=False, **kwargs):
+        Wrapper for the CKAN resource_update API method. See the CKAN API docs for this method to see applicable options
+        (http://docs.ckan.org/en/ckan-2.2/api.html).
+
+        Args:
+          resource_id (string): The id of the resource to update.
+          console (bool, optional): Pretty print the result to the console for debugging. Defaults to False.
+          **kwargs: Any number of optional keyword arguments for the method (see CKAN docs).
+
+        Returns:
+          The response dictionary or None if an error occurs.
+        """
+        # Assemble the data dictionary
+        data = kwargs
+        data['id'] = resource_id
+
+        # Execute
+        result = self._execute_api_request(method='resource_update',
+                                           data_dictionary=data,
+                                           console=console)
+
+        # TODO: The resource_update method only returns a 409 error... address this after resource_create is working. Possible cause is that other parameters may be required...
+
+        return result
+
+    def delete_dataset(self, dataset_id, console=False, **kwargs):
         """
         Delete CKAN dataset
-        :return:
-        """
 
-    def delete_resource(self, console=False, **kwargs):
+        Wrapper for the CKAN package_delete API method. See the CKAN API docs for this method to see applicable options
+        (http://docs.ckan.org/en/ckan-2.2/api.html).
+
+        Args:
+          dataset_id (string): The id or name of the dataset to delete.
+          console (bool, optional): Pretty print the result to the console for debugging. Defaults to False.
+          **kwargs: Any number of optional keyword arguments for the method (see CKAN docs).
+
+        Returns:
+          The response dictionary or None if an error occurs.
+        """
+        # Assemble the data dictionary
+        data = kwargs
+        data['id'] = dataset_id
+
+        # Execute
+        result = self._execute_api_request(method='package_delete',
+                                           data_dictionary=data,
+                                           console=console)
+        return result
+
+    def delete_resource(self, resource_id, console=False, **kwargs):
         """
         Delete CKAN resource
-        :return:
+
+        Wrapper for the CKAN resource_delete API method. See the CKAN API docs for this method to see applicable options
+        (http://docs.ckan.org/en/ckan-2.2/api.html).
+
+        Args:
+          resource_id (string): The id of the resource to delete.
+          console (bool, optional): Pretty print the result to the console for debugging. Defaults to False.
+          **kwargs: Any number of optional keyword arguments for the method (see CKAN docs).
+
+        Returns:
+          The response dictionary or None if an error occurs.
         """
+        # Assemble the data dictionary
+        data = kwargs
+        data['id'] = resource_id
+
+        # Execute
+        result = self._execute_api_request(method='resource_delete',
+                                           data_dictionary=data,
+                                           console=console)
+        return result
