3a1bec4b90cd199451773a6642194fa34e734b50
Nathan Swain <nathan.swain86@gmail.com>
2014-11-06 20:33:23 -0700

    Configuring tests.

--- tethys_datasets/tests/test_config.py
+++ tethys_datasets/tests/test_config.py
@@ -1,18 +1,2 @@
 # Before running tests, enter the credentials of your test services here
 
-TEST_CKAN_DATASET_SERVICE = {
-    'ENGINE': '',
-    'API_ENDPOINT': '',
-    'APIKEY': '',
-    'USERNAME': '',
-    'PASSWORD': ''
-}
-
-TEST_HYDROSHARE_DATASET_SERVICE = {
-    'ENGINE': '',
-    'API_ENDPOINT': '',
-    'APIKEY': '',
-    'USERNAME': '',
-    'PASSWORD': ''
-}
-
